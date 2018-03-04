a3f450ab07b08af3a259039602d48e7c80adab35
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2018-03-04 21:33:09 +0700

    Updates

---
+++ path---404-fd090af65c4ff45c1fc8.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xe70826b53c04],{374:function(a,t){a.exports={data:{site:{siteMetadata:{title:"Kewirausahaan Turfa",subtitle:"Tugas-tugas mata kuliah Kewirausahaan. Ditulis oleh Turfa Auliarachman, 13515133.",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"},{label:"About me",path:"/about/"}],author:{name:"Turfa Auliarachman",email:"turfa_auliarachman@rocketmail.com",telegram:"NangisDarah",twitter:"TurfaGreenbat",github:"#",rss:"#",vk:"#"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-fd090af65c4ff45c1fc8.js.map
